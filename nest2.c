81e10c0b1f477509038deef326eb5e89af3c1d0f
brinda0110 <brindagoyani01@gmail.com>
2025-04-23 14:58:23 +0530

    classwork

---
+++ nest2.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+int main()
+{
+	int a=34 ,b=334,c=133,d=445;
+	if(a>b)
+	{ 
+		if (a>c)
+		{	
+			if(a>d)
+	 			{
+	 			printf("a is Max");
+		 	                        }
+				else 
+				     {
+					 printf("d is max");
+			                             }
+		}
+		        else
+				{
+		        	printf("c is max");
+			                             }
+		
+	}
+	 
+	 
+	
+	
+	else
+	     {if(b>c)
+		     
+			  {if(b>d)
+	                 {
+				        printf("b is Max");
+				                              }
+	else
+	       {
+		      printf("d is Max");
+		                          }
+	              
+}	else
+	          {
+			     printf("c is Max");
+			  }
+	
+}
+	}
+
+		
+	
